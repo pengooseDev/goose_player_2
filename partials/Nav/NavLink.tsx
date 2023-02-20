04c709b20643033592c04ef75011317d1bbc8cd1
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-20 17:01:37 +0900

    feat: NavLink 추가

---
+++ partials/Nav/NavLink.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+interface navLinkProps {
+  value: string;
+  href: string;
+}
+
+const NavLink = ({ href, value }: navLinkProps) => {
+  return (
+    <Link href={href}>
+      <Btn>{value}</Btn>
+    </Link>
+  );
+};
+
+const Btn = styled.div`
+  color: #88ce02;
+  font-weight: 600;
+  padding: 3px;
+  transition: ease-in-out 0.1s;
+  box-sizing: border-box;
+  :hover {
+    border-bottom: 3px solid white;
+    font-weight: 600;
+  }
+`;
+
+export default NavLink;
