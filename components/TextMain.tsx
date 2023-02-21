43501d25ca7bb359f2783b3256123cc51388056b
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-21 18:38:17 +0900

    feat: position 및 CSS transition 변경

--- components/TextMain.tsx
+++ components/TextMain.tsx
@@ -12,8 +12,21 @@ const TextMain = ({ children }: TextMainProps) => {
 export default TextMain;
 
 const Wrapper = styled.div`
+  padding: 50px;
   position: absolute;
-  font-weight: 600;
+  left: 0;
+  bottom: 0;
+  z-index: -10;
+  font-weight: 500;
   font-size: 150px;
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  transition: ease-in-out 2s;
+
+  :nth-last-child(2) :last-child {
+    transition: ease-in-out 0.2s;
+    -webkit-text-stroke: 2px ${({ theme }) => theme.color};
+    color: ${({ theme }) => theme.background};
+    font-size: 100px;
+  }
 `;
