a6f7189ef5d5daa9be5a899849d959914c45c829
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-22 00:46:47 +0900

    chore: Nav CSS 수정

--- partials/Nav/Nav.tsx
+++ partials/Nav/Nav.tsx
@@ -5,35 +5,24 @@ import ToggleBtn from './ThemeToggle';
 const Nav = () => {
   return (
     <Container>
-      <LeftComponents></LeftComponents>
-      <RightComponents>
-        <NavLink value={'Home'} href={'/'} />
-        <NavLink value={'LogIn'} href={'/user/login'} />
-        <NavLink value={'SignIn'} href={'/user/create'} />
-        <ToggleBtn />
-      </RightComponents>
+      <NavLink value={'Home'} href={'/'} />
+      <NavLink value={'LogIn'} href={'/user/login'} />
+      <NavLink value={'SignIn'} href={'/user/create'} />
+      <ToggleBtn />
     </Container>
   );
 };
 
 const Container = styled.div`
+  gap: 15px;
   position: fixed;
-  padding: 10px;
+  margin: 15px;
   top: 0px;
-  right: 0;
+  right: 0px;
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
   height: 25px;
-`;
-
-const RightComponents = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const LeftComponents = styled.div`
-  display: flex;
 `;
 
 export default Nav;
