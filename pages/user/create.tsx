60563ca41f1424a2099dc41bafd0e5da936bbea5
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-21 18:38:55 +0900

    feat: signIn 기본 layout 생성

---
+++ pages/user/create.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import TextMain from '@components/TextMain';
+
+const Create = () => {
+  return (
+    <>
+      <TextMain>
+        <div>Hello.</div>
+        <div>We`re</div>
+        <div>Goose-Player</div>
+      </TextMain>
+      <Wrapper>
+        <Form></Form>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Create;
+
+const Wrapper = styled(motion.div)`
+  position: absolute;
+  width: 400px;
+  height: 600px;
+  border-radius: 5px;
+  left: calc(50% - 200px);
+  top: calc(50% - 300px);
+  color: ${({ theme }) => theme.background};
+  background: rgba(111, 111, 111, 0.5);
+  box-shadow: 0px 0px 5px ${({ theme }) => theme.color};
+  backdrop-filter: blur(3px);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  transition: ease-in-out 0.15s;
+`;
+
+const Form = styled.div``;
