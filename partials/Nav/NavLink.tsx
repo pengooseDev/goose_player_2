001b8a34de58a254192a5d94c793c6e8f892b60b
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-22 00:41:25 +0900

    refactor: Nav 컴포넌트 불필요한 요소 삭제 및 CSS 수정

--- partials/Nav/NavLink.tsx
+++ partials/Nav/NavLink.tsx
@@ -17,10 +17,8 @@ const NavLink = ({ href, value }: navLinkProps) => {
 const Btn = styled.div`
   color: ${({ theme }) => theme.color};
   font-weight: 600;
-  padding: 5px;
   transition: ease-in-out 0.1s;
   box-sizing: border-box;
-  margin: 0 0.5em 0 0.5em;
   height: 30px;
   :hover {
     border-bottom: 3px solid ${({ theme }) => theme.color};
--- partials/Nav/ThemeToggle.tsx
+++ partials/Nav/ThemeToggle.tsx
@@ -8,23 +8,12 @@ const ToggleBtn = () => {
     dispatch(toggle());
   };
 
-  return (
-    <Wrapper>
-      <Color onClick={toggleHandler} />
-    </Wrapper>
-  );
+  return <Color onClick={toggleHandler} />;
 };
 
-const Wrapper = styled.div`
-  width: 30px;
-  height: 30px;
-  padding: 7.5px;
-  border-radius: 3px;
-`;
-
 const Color = styled.div`
-  height: 100%;
-  width: 100%;
+  height: 30px;
+  width: 30px;
   border-radius: 5px;
   background: ${({ theme }) => theme.color};
   transition: 0.15s ease-in-out;
