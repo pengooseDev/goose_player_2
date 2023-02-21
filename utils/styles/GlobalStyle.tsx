df0c01866e577da3fb3e4928dd23dc1f4b891921
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-21 17:15:50 +0900

    chore: Prevent text drag CSS

--- utils/styles/GlobalStyle.tsx
+++ utils/styles/GlobalStyle.tsx
@@ -6,6 +6,20 @@ const GlobalStyle = createGlobalStyle`
     color: ${({ theme }) => theme.color};
     transition: 0.15s ease-in-out;
   }
+  
+  * {
+    -webkit-user-drag: none;
+    -khtml-user-drag: none;
+    -moz-user-drag: none;
+    -o-user-drag: none;
+
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
 `;
 
 export default GlobalStyle;
