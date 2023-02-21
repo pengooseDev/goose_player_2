a2425f7294507d820d878ccfbeae3dcb6351031c
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-21 16:54:27 +0900

    feat: GlobalStyle Theme Toggle Transition 추가

--- utils/styles/GlobalStyle.tsx
+++ utils/styles/GlobalStyle.tsx
@@ -4,6 +4,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme.color};
+    transition: 0.15s ease-in-out;
   }
 `;
 
