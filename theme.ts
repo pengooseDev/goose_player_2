19c2f88e40051721cd160cbc3bcdaf4e02749c27
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-22 16:21:08 +0900

    chore: theme color transparent CSS opacity 변경

--- theme.ts
+++ theme.ts
@@ -2,9 +2,9 @@ import { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = {
   background: '#111',
-  backgroundTransparent: 'rgb(17, 17, 17, 0.65)',
+  backgroundTransparent: 'rgb(17, 17, 17, 0.5)',
   color: '#FADEDD',
-  colorTransparent: 'rgba(250, 222, 221, 0.65)',
+  colorTransparent: 'rgba(250, 222, 221, 0.5)',
   blurBackground: 'rgba(0, 0, 0, 0.5)',
   transitionOption: 'ease-in-out 0.15s',
   imgFilter: 'pink',
@@ -12,10 +12,10 @@ export const darkTheme: DefaultTheme = {
 
 export const lightTheme: DefaultTheme = {
   background: '#FADEDD',
-  backgroundTransparent: 'rgba(250, 222, 221, 0.65)',
+  backgroundTransparent: 'rgba(250, 222, 221, 0.5)',
   color: '#111',
-  colorTransparent: 'rgb(17, 17, 17, 0.65)',
-  blurBackground: 'rgba(255, 255, 255, 0.45)',
+  colorTransparent: 'rgb(17, 17, 17, 0.5)',
+  blurBackground: 'rgba(255, 255, 255, 0.5)',
   transitionOption: 'ease-in-out 0.15s',
   imgFilter: '#111',
 };
