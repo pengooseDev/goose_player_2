8e0381ea1593ae54926bbec7468bf618869d616f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-22 13:55:28 +0900

    feat: Theme field 및 type 추가

--- style.d.ts
+++ style.d.ts
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     background: string;
+    backgroundTransparent: string;
     color: string;
+    colorTransparent: string;
     blurBackground: string;
     transitionOption: string;
     imgFilter: string;
--- theme.ts
+++ theme.ts
@@ -2,7 +2,9 @@ import { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = {
   background: '#111',
+  backgroundTransparent: 'rgb(17, 17, 17, 0.65)',
   color: '#FADEDD',
+  colorTransparent: 'rgba(250, 222, 221, 0.65)',
   blurBackground: 'rgba(0, 0, 0, 0.5)',
   transitionOption: 'ease-in-out 0.15s',
   imgFilter: 'pink',
@@ -10,7 +12,9 @@ export const darkTheme: DefaultTheme = {
 
 export const lightTheme: DefaultTheme = {
   background: '#FADEDD',
+  backgroundTransparent: 'rgba(250, 222, 221, 0.65)',
   color: '#111',
+  colorTransparent: 'rgb(17, 17, 17, 0.65)',
   blurBackground: 'rgba(255, 255, 255, 0.45)',
   transitionOption: 'ease-in-out 0.15s',
   imgFilter: '#111',
