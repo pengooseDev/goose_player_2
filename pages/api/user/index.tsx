31ff158d414269c30a371e8a0800721cb592d1b2
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-23 21:50:43 +0900

    feat: 로그인 구현

--- pages/api/user/index.tsx
+++ pages/api/user/index.tsx
@@ -1,11 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prismaClient from '@/libs/server/client';
-import hashBySort from '@/libs/server/hashing';
+import { decrypt } from '@/libs/server/hashing';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case 'POST':
-      const { id: postNickname, password: postPassword } = req.body;
+      const { id: loginNickname, password: loginPassword } = req.body;
+      const user = await prismaClient.user.findUnique({
+        where: { nickname: loginNickname },
+      });
+
+      if (!user)
+        return res
+          .status(400)
+          .json({ ok: false, errorMessage: 'Not Exist ID' });
+
+      const { password } = user;
+      const isMatch = await decrypt(loginPassword, password);
+      if (!isMatch) return res.status(400).json({ errorMessage: 'Wrong PW' });
 
       return res.status(200).json({ ok: true });
 
