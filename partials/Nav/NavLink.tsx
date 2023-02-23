34f949c53b9d8ce7db9a3d2f90c1d5c34bde64d4
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-24 02:45:11 +0900

    feat: Nav 컴포넌트 Motion 추가

--- partials/Nav/NavLink.tsx
+++ partials/Nav/NavLink.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
 interface navLinkProps {
   value: string;
@@ -7,12 +10,45 @@ interface navLinkProps {
 }
 
 const NavLink = ({ href, value }: navLinkProps) => {
+  const [isHover, setIsHover] = useState(false);
+  const router = useRouter();
+
   return (
-    <Link href={href}>
-      <Btn>{value}</Btn>
-    </Link>
+    <Wrapper
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <Link href={href}>
+        <Btn>{value}</Btn>
+      </Link>
+      <AnimatePresence>
+        {!isHover && router.asPath === href ? (
+          <Dot
+            layoutId={href}
+            variants={hoverVariants}
+            initial="from"
+            animate="to"
+            exit="exit"
+          />
+        ) : isHover ? (
+          <Dash
+            layoutId={href}
+            variants={hoverVariants}
+            initial="from"
+            animate="to"
+            exit="exit"
+          />
+        ) : null}
+      </AnimatePresence>
+    </Wrapper>
   );
 };
+
+export default NavLink;
+
+const Wrapper = styled.div`
+  position: relative;
+`;
 
 const Btn = styled.div`
   color: ${({ theme }) => theme.color};
@@ -20,9 +56,29 @@ const Btn = styled.div`
   transition: ease-in-out 0.1s;
   box-sizing: border-box;
   height: 30px;
-  :hover {
-    border-bottom: 3px solid ${({ theme }) => theme.color};
-  }
 `;
 
-export default NavLink;
+const Dot = styled(motion.div)`
+  position: absolute;
+  left: calc(50% - 10%);
+  bottom: 0px;
+  width: 20%;
+  height: 3px;
+  border-radius: 3px;
+  background: ${({ theme }) => theme.color};
+`;
+
+const Dash = styled(motion.div)`
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 3px;
+  border-radius: 3px;
+  background: ${({ theme }) => theme.color};
+`;
+
+const hoverVariants = {
+  from: { opacity: 1 },
+  to: { opacity: 1, transition: { duration: 0.01 } },
+  exit: { opacity: 0, transition: { duration: 0.15 } },
+};
