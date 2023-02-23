9ce2873e026fa323cf314823820cd48636f449df
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-23 18:35:37 +0900

    feat: 비밀번호 해싱기능 추가

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -1,30 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prismaClient from '@/libs/server/client';
+import hashBySort from '@/libs/server/hashing';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case 'POST':
       const { id: postNickname, password: postPassword } = req.body;
-
-      const isDuplicateName = !!(await prismaClient.user.findUnique({
-        where: { nickname: postNickname },
-      }));
-
-      if (isDuplicateName)
+      const isDuplicateName = await checkDuplicateByNickname(postNickname);
+      if (isDuplicateName) {
         return res
           .status(409)
-          .redirect('/user/create')
-          .json({ error: 'Duplicated ID!' });
+          .json({ ok: false, errorMessage: 'Duplicated ID' });
+      }
+
+      const hashedPassword = await hashBySort(postPassword, 10);
 
       const newUser = await prismaClient.user.create({
         data: {
           nickname: postNickname,
-          password: postPassword,
+          password: hashedPassword,
         },
       });
 
-      console.log(newUser);
-      return res.status(200).json({ ok: true, data: '성공! 받아온 값입니다' });
+      return res.status(200).json({ ok: true, data: newUser.nickname });
 
     default:
       return res.status(500).end();
